ca9b4c372e51554a1b6fcba979af4a12f1b983d3
Scott Meadows <Scottmeadows04@gmail.com>
2022-11-09 17:32:59 -0500

    add full functionality

--- src/index.js
+++ src/index.js
@@ -11,14 +11,35 @@ const DICE_IMAGES = {
 
 const diceContainer = document.getElementById('dice-container');
 const rollDiceForm = document.getElementById('roll-dice-form');
+const handForm = document.getElementById('hand-select-form');
 const rollsThisTurnDisplay = document.getElementById('rolls-left');
 const handSelector = document.getElementById('hand-select');
 const currentPoints = document.getElementById('current-points');
 const diceSubmitButton = document.querySelector('#roll-dice-form input[type="submit"]');
 const handSubmitButton = document.querySelector('#hand-select-form input[type="submit"]');
+const scorecardTable = document.querySelector('#scorecard tbody').children;
+const totalScoreDisplay = document.getElementById('total-points');
+
 
 const dice = [new Die(6), new Die(6), new Die(6), new Die(6), new Die(6)];
 let rollsThisTurn = 3;
+
+let totalScore = 0;
+const scorecard = {
+    aces: {score: 0, used: false},
+    deuces: {score: 0, used: false},
+    threes: {score: 0, used: false},
+    fours: {score: 0, used: false},
+    fives: {score: 0, used: false},
+    sixes: {score: 0, used: false},
+    threeOfAKind: {score: 0, used: false},
+    fourOfAKind: {score: 0, used: false},
+    fullHouse: {score: 0, used: false},
+    smallStraight: {score: 0, used: false},
+    largeStraight: {score: 0, used: false},
+    yahtzee: {score: 0, used: false},
+    chance: {score: 0, used: false}
+}
 
 rollDiceForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -30,9 +51,15 @@ rollDiceForm.addEventListener('submit', e => {
         5: e.target['freeze-die-5'].checked,
     }
     dice.forEach((die, index) => {
-        if (!keepDice[index + 1])
-            die.roll;
+        if (!keepDice[index + 1]) {
+            die.roll
+            // test yahtzee bonus
+            // die.currentSide = 1;
+        }
     });
+    if (rollsThisTurn === 3) {
+        toggleKeepDisabled();
+    }
     rollsThisTurn--;
     rollsThisTurnDisplay.textContent = rollsThisTurn;
     if (rollsThisTurn === 0) {
@@ -44,6 +71,12 @@ rollDiceForm.addEventListener('submit', e => {
         handSubmitButton.toggleAttribute('disabled');
     }
     loadDice();
+    if (checkForYahtzeeBonus()) {
+        console.log('yahtzee bonus!')
+        //yahtzee bonus message!
+        totalScore += 100;
+        // reset rolls, score
+    }
 })
 
 handSelector.addEventListener('change', (e) => {
@@ -51,9 +84,18 @@ handSelector.addEventListener('change', (e) => {
     if (e.target.value === '')
         potentialPoints = 0;
     else
-        potentialPoints = HAND_TYPES[e.target.value].score(dice);
+        potentialPoints = HAND_TYPES[dashesToCamelCase(e.target.value)].score(dice);
     currentPoints.textContent = potentialPoints;
 });
+
+handForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const hand = dashesToCamelCase(e.target['hand-select'].value);
+    if (hand === '') 
+        return;
+    submitHand(hand);
+    e.target.reset();
+})
 
 function loadDice() {
     dice.forEach((die, index) => {
@@ -66,11 +108,88 @@ function loadDice() {
     })
 }
 
-loadDice();
+function submitHand(hand) {
+    const scoreThisHand = HAND_TYPES[hand].score(dice);
+    // submit the current hand and add points
+    totalScore += scoreThisHand
+    scorecard[hand].score = scoreThisHand;
+    scorecard[hand].used = true;
+    // update the scorecard
+    const handTableRow = document.getElementById(`scorecard-${camelCaseToDashes(hand)}`);
+    const handScore = handTableRow.querySelector('.score');
+    handTableRow.classList.add('scored');
+    handScore.textContent = scoreThisHand;
+    // update the total points display
+    totalScoreDisplay.textContent = totalScore;
+    // remove the hand from the select list
+    const handSelection = document.getElementById(camelCaseToDashes(hand));
+    handSelection.remove();
+    // disable select and re-enable roll
+    handSubmitButton.classList.toggle('disabled');
+    handSubmitButton.toggleAttribute('disabled');
+    if(diceSubmitButton.classList.contains('disabled')) {
+        diceSubmitButton.toggleAttribute('disabled');
+        diceSubmitButton.classList.toggle('disabled');
+    }
+    // reset roll count
+    rollsThisTurn = 3;
+    rollsThisTurnDisplay.textContent = rollsThisTurn;
+    // reset score preview
+    currentPoints.textContent = 0;
+    // disable keeps
+    toggleKeepDisabled();
+    uncheckAllKeep();
+    // check for win
+    if (handSelector.children.length === 1) {
+        // end game, tally score
+        tallyFinalScores();
+    }
+}
 
-/**
- * Roll The Dice:
- *  1. must roll each die to be a new random die
- *  2. must decrement the rolls remaining
- *  3. must not roll the dice that are marked as reserved.
- */
+function toggleKeepDisabled() {
+    for (let i = 1; i <= 5; i++) {
+        const keepCheckbox = document.getElementById(`freeze-die-${i}`);
+        keepCheckbox.toggleAttribute('disabled');
+    }
+}
+
+function uncheckAllKeep() {
+    for (let i = 1; i <= 5; i++) {
+        const keepCheckbox = document.getElementById(`freeze-die-${i}`);
+        keepCheckbox.checked = false;
+    }
+}
+
+function dashesToCamelCase(str) {
+    return str.split('-').map((element, index) => {
+        if (index !== 0)
+            return element[0].toUpperCase() + element.substring(1);
+        else
+            return element;
+    }).join('');
+}
+
+function camelCaseToDashes(str) {
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (char === char.toUpperCase()) {
+            str = str.substring(0, i) + '-' + char.toLowerCase() + str.substring(i + 1);
+        }
+    }
+    return str;
+}
+
+function tallyFinalScores() {
+    if (scorecard.aces.score + scorecard.deuces.score + scorecard.threes.score + scorecard.fours.score + scorecard.fives.score + scorecard.sixes.score >= 63) {
+        totalScore += 35;
+    }
+    
+    console.log(totalScore);
+}
+
+// on any given roll, check if a yahtzee bonus occurs
+function checkForYahtzeeBonus() {
+    return (HAND_TYPES.yahtzee.isValid(dice) && scorecard.yahtzee.used)
+}
+
+loadDice();
--- src/objects.js
+++ src/objects.js
@@ -15,29 +15,137 @@ class Die {
     }
 }
 
+class Hand {
+    constructor(scoreMethod, isValid) {
+        this.scoreMethod = scoreMethod;
+        this.isValid = isValid;
+    }
+
+    score(dice) {
+        if (this.isValid(dice)) {
+            return this.scoreMethod(dice);
+        } else {
+            return 0;
+        }
+    }
+}
+
 const HAND_TYPES = {
-    aces: {
-        score: addAllOfType(1)
-    },
-    deuces: {
-        score: addAllOfType(2)
-    },
-    threes: {
-        score: addAllOfType(3)
-    },
-    fours: {
-        score: addAllOfType(4)
-    },
-    fives: {
-        score: addAllOfType(5)
-    },
-    sixes: {
-        score: addAllOfType(6)
-    },
+    aces: new Hand(addAllOfType(1), returnTrue),
+    deuces: new Hand(addAllOfType(2), returnTrue),
+    threes: new Hand(addAllOfType(3), returnTrue),
+    fours: new Hand(addAllOfType(4), returnTrue),
+    fives: new Hand(addAllOfType(5), returnTrue),
+    sixes: new Hand(addAllOfType(6), returnTrue),
+    threeOfAKind: new Hand(totalDice, hasXOfAKind(3)),
+    fourOfAKind: new Hand(totalDice, hasXOfAKind(4)),
+    fullHouse: new Hand(returnNumber(25), (dice) => {
+        const hash = diceToHash(dice);
+        return (hashContains(hash, 2) && hashContains(hash, 3));
+    }),
+    smallStraight: new Hand(returnNumber(30), hasStraightOfLength(4)),
+    largeStraight: new Hand(returnNumber(40), hasStraightOfLength(5)),
+    yahtzee: new Hand(returnNumber(50), hasXOfAKind(3)),
+    chance: new Hand(totalDice, returnTrue)
+}
+
+function returnTrue() {
+    return true;
+}
+
+function returnNumber(number) {
+    return () => number;
 }
 
 function addAllOfType(number) {
     return function(dice) {
         return dice.filter((element) => element.currentSide === number).reduce((sum, element) => sum + element.currentSide, 0);
     };
+}
+
+function totalDice(dice) {
+    return dice.reduce((sum, element) => sum + element.currentSide, 0);
+}
+
+function hasXOfAKind(x) {
+    return function(dice) {
+        const hash = diceToHash(dice);
+        for (let side in hash) {
+            if (hash[side] >= x)
+                return true;
+        }
+        return false;
+    }
+}
+
+function diceToHash(dice) {
+    const hash = {};
+        for (let die of dice) {
+            if (hash[die.currentSide])
+                hash[die.currentSide]++;
+            else
+                hash[die.currentSide] = 1;
+        }
+    return hash;
+}
+
+function hashContains(hash, number) {
+    for (let side in hash) {
+        if (hash[side] === number)
+            return true;
+    }
+    return false;
+}
+
+function hasStraightOfLength(length) {
+    return function(dice) {
+        const hash = {};
+        dice = dice.filter((element) => {
+            if (hash[element.currentSide] !== undefined)
+                return false;
+            else {
+                hash[element.currentSide] = true;
+                return true;
+            }
+        });
+        const numDice = toNumberArray(dice);
+        let longestStraight = 1;
+        let currentStraight = 1;
+        sortDice(numDice);
+        let lastNumber = numDice[0];
+        for (let i = 1; i < numDice.length; i++) {
+            if (numDice[i] === lastNumber + 1) {
+                currentStraight++;
+                if (currentStraight > longestStraight) 
+                    longestStraight = currentStraight;
+            } else {
+                currentStraight = 1;
+            }
+            lastNumber = numDice[i];
+        }
+        return (longestStraight >= length);
+    }
+}
+
+function sortDice(dice) {
+    for (let i = 0; i < dice.length; i++) {
+        const minIndex = minIndexFrom(dice, i);
+        const temp = dice[i];
+        dice[i] = dice[minIndex];
+        dice[minIndex] = temp;
+    }
+    return dice;
+}
+
+function toNumberArray(dice) {
+    return dice.map((element) => element.currentSide);
+}
+
+function minIndexFrom(arr, startIndex) {
+    let minIndex = startIndex;
+    for (let i = startIndex; i < arr.length; i++) {
+        if (arr[i] < arr[minIndex])
+            minIndex = i;
+    }
+    return minIndex;
 }
